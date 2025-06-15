25c923f2dd6d4d9be1330e1ca98b9d8d05aaf8c4
Vaibhav <mish0032@algonquinlive.com>
2025-06-15 19:13:58 -0400

    Added TFLint with strict rules and fixed issues

--- test/azure_webserver_test.go
+++ test/azure_webserver_test.go
@@ -8,38 +8,43 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-// You normally want to run this under a separate "Testing" subscription
-// For lab purposes you will use your assigned subscription under the Cloud Dev/Ops program tenant
-var subscriptionID string = "<your-subscription-id>"
+// Your Azure subscription ID
+var subscriptionID string = "eed2085b-133e-4466-8ed6-6da86c403fc0"
 
 func TestAzureLinuxVMCreation(t *testing.T) {
+	// Define the prefix to match your Terraform variable
+	labelPrefix := "lab6" 
+
+	// Terraform options for Terratest
 	terraformOptions := &terraform.Options{
-		// The path to where our Terraform code is located
 		TerraformDir: "../",
-		// Override the default terraform variables
 		Vars: map[string]interface{}{
-			"labelPrefix": "<your-college-username>",
+			"label_prefix": labelPrefix, 
+		},
+		EnvVars: map[string]string{
+			"ARM_SUBSCRIPTION_ID": subscriptionID,
 		},
 	}
 
+	// Clean up resources with `terraform destroy` after the test
 	defer terraform.Destroy(t, terraformOptions)
 
-	// Run `terraform init` and `terraform apply`. Fail the test if there are any errors.
+	// Run `terraform init` and `terraform apply`
 	terraform.InitAndApply(t, terraformOptions)
 
-	// Run `terraform output` to get the value of output variable
+	// Get Terraform output values
 	vmName := terraform.Output(t, terraformOptions, "vm_name")
 	resourceGroupName := terraform.Output(t, terraformOptions, "resource_group_name")
 	nicName := terraform.Output(t, terraformOptions, "nic_name")
 
-	// Confirm VM exists
+	// Validate the VM exists
 	assert.True(t, azure.VirtualMachineExists(t, vmName, resourceGroupName, subscriptionID))
 
-	// Confirm NIC exists and is connected to VM
+	// Validate NIC is attached to VM
 	actualNicNames := azure.GetVirtualMachineNics(t, vmName, resourceGroupName, subscriptionID)
 	assert.Equal(t, nicName, actualNicNames[0])
 
-	// Confirm the VM is running the correct Ubuntu version
+	// Validate correct image is used
 	vmImage := azure.GetVirtualMachineImage(t, vmName, resourceGroupName, subscriptionID)
 	expectedOSPublisher := "Canonical"
 	expectedOSVersion := "22_04-lts-gen2"
